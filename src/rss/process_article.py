a16e8e6e222bf05129effb65ede4df6c9730b884
robaldis <robertaldis@gmail.com>
2020-12-03 19:40:42 +0000

    Cleaning up code

--- src/rss/process_article.py
+++ src/rss/process_article.py
@@ -1,29 +1,34 @@
-
+#process_article.py 
 from bs4 import BeautifulSoup
 import requests
+    
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+    
+def article_to_string(url):
+    """
+    Process the body of the article from the given url
+    """
+    #NOTE: this may need to be chagned for different ways different sites lay
+    # the website out.
 
-class ProcessArticle():
-    def __init__(self):
-        pass
-
-    def setUrl(self, url):
-        # get the body of the text from the RSS url
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        p = soup.article.find_all('p')
-        finalString = "" 
-        for s in p:
-            try:    
-                finalString += s.string
-            except:
-                pass
-        return finalSString
-
-# give a URL
-# process the url to get the right data
-# process that data to get to a cosine silimarity matrix 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    # find all the p tages int the article tag. 
+    p = soup.article.find_all('p')
+    finalString = "" 
+    for s in p:
+        try:    
+            finalString += s.string
+        except:
+            # This tag has no text, the text is probably in a child tag
+            # this is usualy text with styling
+            # NOTE: This may need to changed to make the recomendations work better
+            pass
+    return finalString
 
 
+# Testing
 if __name__ == "__main__":
     p = ProcessArticle()
     string = p.setUrl("https://www.bbc.co.uk/news/uk-england-bristol-55173667")
